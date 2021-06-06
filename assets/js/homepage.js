7f43a02998dce699c93e0c70db54032f415ece42
Joseph <joe@uhhuhohyeah.com>
2021-06-06 09:04:51 -0700

    conformed to the code snapshot

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,9 +1,8 @@
 var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
-var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -23,22 +22,17 @@ var formSubmitHandler = function(event) {
   }
 };
 
-var getFeaturedRepos = function(language) {
-  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+var buttonClickHandler = function(event) {
+  // get the language attribute from the clicked element
+  var language = event.target.getAttribute("data-language");
 
-  fetch(apiUrl).then(function(response) {
-    if (response.ok) {
-      response.json().then(function(data) {
-        displayRepos(data.items, language);
-      });
-    } else {
-      alert('Error: GitHub User Not Found');
-    }
-  });
+  if (language) {
+    getFeaturedRepos(language);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
 };
-
-
-
 
 var getUserRepos = function(user) {
   // format the github api url
@@ -61,6 +55,23 @@ var getUserRepos = function(user) {
     .catch(function(error) {
       alert("Unable to connect to GitHub");
     });
+};
+
+var getFeaturedRepos = function(language) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
 };
 
 var displayRepos = function(repos, searchTerm) {
@@ -109,17 +120,6 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
-var buttonClickHandler = function(event) {
-  var language = event.target.getAttribute("data-language");
-  console.log(language);
-  if (language) {
-    getFeaturedRepos(language);
-  
-    // clear old content
-    repoContainerEl.textContent = ""; // clears out text from the repo container
-  }
-};
-
-// add event listeners to forms
+// add event listeners to form and button container
 userFormEl.addEventListener("submit", formSubmitHandler);
 languageButtonsEl.addEventListener("click", buttonClickHandler);
